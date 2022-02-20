3b0bad7e3c7a7ceb79a11b002bcc907a74c39d21
jskhds <2987163443a@gmail.com>
2022-02-20 22:44:59 +0800

    welcome页面

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     host:'localhost',
     port:8080,
-    // 配置前端的代理，从后端地址 http://localhost:3000 来
+    // 配置前端的代理，从后端地址，遇到api 就定向到 http://localhost:3000 来
     proxy:{
       "/api":{
         target:"http://localhost:3000"
