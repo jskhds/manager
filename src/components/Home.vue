b74d5b12fc284723346b33a5343dc5db01c7c13e
jskhds <2987163443a@gmail.com>
2022-02-20 23:18:16 +0800

    Home组件的静态实现

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,62 @@
 <template>
   <div class="basic-layout">
-    <div class="nav-side"></div>
-    <div class="content-right">
+    <!-- 注意控制的是侧边栏的展开，同时还要让右边的区域宽度动态变化 -->
+    <div :class="['nav-side',isCollapse?'fold':'unfold']">
+      <!-- 系统LOGO -->
+      <div class="logo">
+        <img src="./../assets/logo.png">
+        <span>Manager</span>
+      </div>
+      <!-- 导航菜单 -->
+      <el-menu
+        default-active="2"
+        background-color="#001529"
+        text-color="#fff"
+        router
+        :collapse="isCollapse"
+        class="nav-menu">
+        <el-submenu index="1">
+          <template #title>
+            <i class="el-icon-setting"></i>
+            <span>系统管理</span>
+          </template>
+          <el-menu-item index="1-1">用户管理</el-menu-item>
+          <el-menu-item index="1-2">菜单管理</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+          <template #title>
+            <i class="el-icon-setting"></i>
+            <span>审批管理</span>
+          </template>
+          <el-menu-item index="2-1">休假申请</el-menu-item>
+          <el-menu-item index="2-2">待我审批</el-menu-item>
+        </el-submenu>
+    </el-menu>
+    </div>
+    <!-- 如果侧边栏收起和展开，宽度随之改变 -->
+    <div :class="['content-right',isCollapse?'fold':'unfold']">
       <div class="nav-top">
-        <div class="bread">面包屑</div>
-        <div class="user-">用户</div>
+        <div class="nav-left">
+          <div class="menu-fold" @click="toggle"> <i class="el-icon-s-fold"></i></div>           
+            <div class="bread">面包屑</div>
+        </div>
+        <div class="user-info">
+          <el-badge :is-dot="true" class="notice" type="danger">
+            <i class="el-icon-bell"></i>
+          </el-badge>
+          <el-dropdown @command="handleLogout">
+            <span class="user-link">
+              {{userInfo.userName}}
+              <i class="el-icon--right"></i>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item command="email">邮箱:{{userInfo.userEmail}}</el-dropdown-item>
+                <el-dropdown-item command="logout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </div>
       </div>
       <div class="wrapper">
         <div class="main-page">
@@ -18,6 +70,26 @@
 <script>
 export default{
   name:'Home',
+  data(){
+    return {
+      isCollapse:false,
+      userInfo:{
+        userName:'Dmoon',
+        userEmail:'2987163443@gmail.com'
+      }
+    }
+  },
+  methods:{
+    toggle(){
+      this.isCollapse = !this.isCollapse;
+    },
+    handleLogout(key){
+      if(key == 'email')return;
+      this.$store.commit('saveUserInfo','');
+      this.userInfo = null;
+      this.$router.push('/login');
+    }
+  }
 }
 </script>
 
@@ -31,10 +103,41 @@ export default{
     background-color: #001529;
     color:#fff;
     overflow-y: auto;
-    transition: width .5s;
+    transition: width .5s;  // 在侧边栏合并展开的时候添加过渡动画
+    .logo{
+      display: flex;
+      align-items: center;
+      font-size: 18px;
+      height:50px;
+      img{
+        margin:0 16px;
+        width:32px;
+        height:32px;
+      }
+    }
+    .nav-menu{
+      height:calc(100vh - 50px);
+      border-right:none;
+    }
+    // 合并
+    &.fold{
+      width:64px;
+    }
+    // 展开
+    &.unfold{
+      width:200px;
+    }
   }
   .content-right{
     margin-left:200px;
+    // 合并
+    &.fold{
+      margin-left:64px;
+    }
+    // 展开
+    &.unfold{
+      margin-left:200px;
+    }
     .nav-top{
       height: 50px;
       line-height: 50px;
@@ -42,6 +145,24 @@ export default{
       justify-content: space-between;
       border-bottom: 1px solid #ddd;
       padding: 0 20px;
+      .nav-left{
+        display: flex;
+        align-items: center;
+        .menu-fold{
+          margin-right:15px;
+          font-size: 18px;
+        }
+      }
+      .user-info{
+        .notice{
+          line-height:30px;
+          margin-right:15px;
+        }
+        .user-link{
+          cursor: pointer;
+          color:#409eff;
+        }
+      }
     }
     .wrapper{
       background: #eef0f3;
